807c62fd4e654b49529a22a7cb6f5d48e9a50b7d
Michelle Poterek <poterekm@gmail.com>
2017-05-03 11:52:14 -0700

    add general format for user to select responses to question via drop down menu instead of radio buttons.

--- week2/js/scripts.js
+++ week2/js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 
 
-var name =("Please enter your first name:")
+var userName = $("input#userName").val();
+var question1 = parseInt($("#question1").val());
+var question2 = parseInt($("#question1").val());
+
+
 
 });
