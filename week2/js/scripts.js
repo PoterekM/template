aea6f963cfae7673f05f879f6d1c4aab4723e903
Michelle Poterek <poterekm@gmail.com>
2017-05-03 13:05:42 -0700

    add form id to the questions aspect as well as fix question id's for form groups.

--- week2/js/scripts.js
+++ week2/js/scripts.js
@@ -1,10 +1,19 @@
+var add = function(question1, question2, question3, question4, question5) {
+  return question1 + question2+ question3 + question4 + question5;
+};
+
 $(document).ready(function() {
 
+  $("form#Questions").submit(function(event){
+  var userName = $("input#userName").val();
+  var question1 = parseInt($("#question1").val());
+  var question2 = parseInt($("#question2").val());
+  var question3 = parseInt($("#question3").val());
+  var question4 = parseInt($("#question4").val());
+  var question5 = parseInt($("#question5").val());
+  var userDestiny = add(question1,question2,question3,question4,question5)
 
-var userName = $("input#userName").val();
-var question1 = parseInt($("#question1").val());
-var question2 = parseInt($("#question1").val());
 
 
-
+  });
 });
